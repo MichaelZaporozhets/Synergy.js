d1d635457983a55a04a214f39f7dca5689739663
Michael Zaporozhets <michaelzaporozhets@gmail.com>
2014-05-27 17:24:27 +1000

    usage

--- synergy.js
+++ synergy.js
@@ -1,3 +1,24 @@
+
+//  __                                     _     
+// / _\_   _ _ __   ___ _ __ __ _ _   _   (_)___ 
+// \ \| | | | '_ \ / _ \ '__/ _` | | | |  | / __|
+// _\ \ |_| | | | |  __/ | | (_| | |_| |_ | \__ \
+// \__/\__, |_| |_|\___|_|  \__, |\__, (_)/ |___/
+//     |___/                |___/ |___/ |__/     
+//
+// Come synergize with me...
+// 
+//
+// USAGE:
+//
+// "I want to enlarge business growth by the end of the year.".synergize();
+//
+// OR
+//
+// var synergizedText = synergize("I want to enlarge business growth by the end of the year.")
+//
+
+
 (function(exports) {
 	var synergizer = function() {
 		this.synergaurus = [
